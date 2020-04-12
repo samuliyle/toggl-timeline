d3b0f641557a2f40d4ed5ea9695ea618c933aa07
Samuli Ylenius <wraithyzz@gmail.com>
2020-04-12 16:01:44 +0300

    Add TogglController & model

---
+++ Controllers/TogglController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using toggl_timeline.Models;
+
+namespace toggl_timeline.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TogglController : ControllerBase
+    {
+        private readonly IHttpClientFactory clientFactory;
+        private readonly Key ApiKeys;
+
+        public TogglController(IHttpClientFactory clientFactory, IOptions<Key> apiKeys)
+        {
+            this.clientFactory = clientFactory;
+            ApiKeys = apiKeys.Value;
+        }
+
+        [HttpGet("GetDetailedReport")]
+        public async Task<ActionResult<ToggleDetailedReport>> GetDetailedReport()
+        {
+            var url = "details";
+            url += $"?workspace_id={ApiKeys.WorkSpaceId}";
+            url += $"&since={DateTime.UtcNow.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture)}";
+            url += "&user_agent=api_test";
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var client = clientFactory.CreateClient("toggl");
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<ToggleDetailedReport>(content);
+            } else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ Models/Toggl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace toggl_timeline.Models
+{
+
+    public class ToggleDetailedReport
+    {
+        public int total_count { get; set; }
+        public int per_page { get; set; }
+        public List<Data> data { get; set; }
+    }
+
+    public class Data
+    {
+        public long id { get; set; }
+        public long pid { get; set; }
+        public long? tid { get; set; }
+        public long uid { get; set; }
+        public string description { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+        public DateTime updated { get; set; }
+        public long dur { get; set; }
+        public string user { get; set; }
+        public string project { get; set; }
+        public string project_color { get; set; }
+        public string project_hex_color { get; set; }
+        public List<string> tags { get; set; }
+    }
+
+}
