b8529c6cedbcddec6e8c3a18cad233cfb51e7daf
Samuli Ylenius <wraithyzz@gmail.com>
2020-04-18 15:49:41 +0300

    Remove unused files

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import Toggl from './components/Toggl';
 
 import './custom.css'
@@ -14,10 +11,7 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-        <Route path='/toggl1' component={Toggl} />
+        <Route exact path='/' component={Toggl} />
       </Layout>
     );
   }
