588288e30359ca442e2c0c30ad4951d9f3ce2d10
Samuli Ylenius <wraithyzz@gmail.com>
2020-04-23 16:02:04 +0300

    Remove unused properties from toggl data model class

--- Models/Toggl.cs
+++ Models/Toggl.cs
@@ -24,19 +24,12 @@ namespace toggl_timeline.Models
     public class Data
     {
         public long id { get; set; }
-        public long pid { get; set; }
-        public long? tid { get; set; }
-        public long uid { get; set; }
         public string description { get; set; }
         public DateTime start { get; set; }
         public DateTime end { get; set; }
-        public DateTime updated { get; set; }
         public long dur { get; set; }
         public string user { get; set; }
         public string project { get; set; }
-        public string project_color { get; set; }
-        public string project_hex_color { get; set; }
         public List<string> tags { get; set; }
     }
-
 }
